8168408bc2d0053d832d90e51afcc908344007a1
Matheus Soares <mathe_oare@hotmail.com>
2018-05-02 23:10:47 -0300

    allow empty values in dotenv

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ const requireProcessEnv = (name) => {
 if (process.env.NODE_ENV !== 'production' && process.env.HEROKU_ENV !== 'active') {
   const dotenv = require('dotenv-safe')
   dotenv.load({
+    allowEmptyValues: true,
     path: path.join(__dirname, '../.env'),
     sample: path.join(__dirname, '../.env.example')
   })
